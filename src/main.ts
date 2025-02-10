11e871fd4bb2b75709cc5f57e908d4091b2c1439
Vlaad1955 <wylf1312@gmail.com>
2025-02-10 12:33:01 +0200

    add Swaggetr

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as process from 'process';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const config = new DocumentBuilder()
+      .setTitle('Nest bonus module')
+      .setDescription('The may-2024 API description')
+      .setVersion('1.0')
+      .addTag('okten')
+      .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
